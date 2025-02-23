41fc6b58d9610bae3f3c58f216fdfcfa5a748186
FernandoChocKinal <fchoc-2020619@kinal.edu.gt>
2025-02-23 14:24:59 -0600

    Subiendo archivos y carpetas a la rama develop

---
+++ configs/server.js
@@ -0,0 +1,58 @@
+"use strict"
+
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { dbConnection } from "./mongo.js";
+import { adminPorDefault } from "../src/user/user.controller.js";
+import { categoriaPorDefecto } from "../src/categoria/categoria.controller.js";
+import authRoutes from "../src/auth/auth.routes.js";
+import userRoutes from "../src/user/user.routes.js";
+import publicacionesRoutes from "../src/publicaciones/publicaciones.routes.js";
+import categoriaRoutes from "../src/categoria/categoria.routes.js";
+import comentarioRoutes from "../src/comentario/comentario.routes.js";
+import apiLimiter from "../src/middlewares/rate-limit-validator.js";
+import { swaggerDocs, swaggerUi } from "./swagger.js";
+
+const middlewares = (app) => {
+    app.use(express.urlencoded({extended: false}))
+    app.use(express.json())
+    app.use(cors())
+    app.use(helmet())
+    app.use(morgan("dev"))
+    app.use(apiLimiter)
+}
+
+const routes = (app) =>{
+    app.use("/gestorDeOpiniones/v1/auth", authRoutes);
+    app.use("/gestorDeOpiniones/v1/User", userRoutes);
+    app.use("/gestorDeOpiniones/v1/Categoria", categoriaRoutes);
+    app.use("/gestorDeOpiniones/v1/Publicacion", publicacionesRoutes);
+    app.use("/gestorDeOpiniones/v1/Comentario", comentarioRoutes);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs))
+}
+
+const conectarDB = async () =>{
+    try{
+        await dbConnection()
+        await adminPorDefault()
+        await categoriaPorDefecto()
+    }catch(err){
+        console.log(`Database connection failed: ${err}`)
+        process.exit(1)
+    }
+}
+
+export const initServer = () => {
+    const app = express()
+    try{
+        middlewares(app)
+        conectarDB()
+        routes(app)
+        app.listen(process.env.PORT)
+        console.log(`Server running on port ${process.env.PORT}`)
+    }catch(err){
+        console.log(`Server init failed: ${err}`)
+    }
+};
---
+++ src/auth/auth.routes.js
@@ -0,0 +1,76 @@
+import { Router } from "express";
+import { register, login } from "./auth.controller.js";
+import { registerValidator, loginValidator } from "../middlewares/user-validators.js";
+
+const router = Router();
+
+    /**
+     * @swagger
+     * /register:
+     *   post:
+     *     summary: Register a new user
+     *     consumes:
+     *       - multipart/form-data
+     *     parameters:
+     *       - in: formData
+     *         name: username
+     *         type: string
+     *         required: true
+     *         description: The username of the user
+     *       - in: formData
+     *         name: password
+     *         type: string
+     *         required: true
+     *         description: The password of the user
+     *     responses:
+     *       201:
+     *         description: User registered successfully
+     *       400:
+     *         description: Bad request
+     *       500:
+     *         description: Internal server error
+     */
+    router.post(
+        "/register",
+        registerValidator,
+        register
+    );
+    
+    /**
+     * @swagger
+     * /login:
+     *   post:
+     *     summary: Login a user
+     *     consumes:
+     *       - application/json
+     *     parameters:
+     *       - in: body
+     *         name: user
+     *         description: The user to login
+     *         schema:
+     *           type: object
+     *           required:
+     *             - username
+     *             - password
+     *           properties:
+     *             username:
+     *               type: string
+     *             password:
+     *               type: string
+     *     responses:
+     *       200:
+     *         description: User logged in successfully
+     *       400:
+     *         description: Bad request
+     *       401:
+     *         description: Unauthorized
+     *       500:
+     *         description: Internal server error
+     */
+    router.post(
+        "/login",
+        loginValidator,
+        login
+    );
+    
+export default router;
