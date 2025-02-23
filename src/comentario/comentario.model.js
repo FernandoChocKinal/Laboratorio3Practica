ef8235cd9442441a5f4915d8e401942639ae0382
FernandoChocKinal <fchoc-2020619@kinal.edu.gt>
2025-02-23 14:15:04 -0600

    Agregar 'comentario' a la rama f/comentarios

---
+++ src/comentario/comentario.model.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+const ComentarioSchema = new Schema({
+    texto: {
+        type: String,
+        maxLength: [250, "El texto no debe exceder los 250 caracteres"]
+    },
+    autor: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: [true, "El autor es obligatorio"]
+    },
+    publicacion: {
+        type: Schema.Types.ObjectId,
+        ref: "Publicacion",
+        required: [true, "La publicación es obligatoria"]
+    }
+}, {
+    versionKey: false,
+    timestamps: true
+});
+
+export default model("Comentario", ComentarioSchema);
